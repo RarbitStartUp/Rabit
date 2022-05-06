e64e57216b03eaef2751de5e4fa306ede2142da4
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-06 14:22:27 +0800

    adding a tag to activate clickable card component

--- pages/shops/Shops.js
+++ pages/shops/Shops.js
@@ -22,21 +22,22 @@ export default function Shops() {
 
   return (
     <div className="flex">
-      <div className="mx-auto flex flex-col justify-center space-y-3">
+      <div className="mx-auto flex-col space-y-3">
         {shops.map((shop) => (
           <Link href={'/shops/' + shop.id} key={shop.id}>
-            {/* <h1>{shop.data().shopName}</h1> */}
-            <Card
-              key={shop.id}
-              shopName={shop.data().shopName}
-              categories={shop.data().categories}
-              location={shop.data().location}
-              rewardPoints={shop.data().rewardPoints}
-              tokenAllocations={shop.data().tokenAllocations}
-              imageUrl={shop.data().imageUrl}
-              timestamp={shop.data().timestamp}
-              email={shop.data().email}
-            />
+            <a>
+              <Card
+                key={shop.id}
+                shopName={shop.data().shopName}
+                categories={shop.data().categories}
+                location={shop.data().location}
+                rewardPoints={shop.data().rewardPoints}
+                tokenAllocations={shop.data().tokenAllocations}
+                imageUrl={shop.data().imageUrl}
+                timestamp={shop.data().timestamp}
+                email={shop.data().email}
+              />
+            </a>
           </Link>
         ))}
       </div>
