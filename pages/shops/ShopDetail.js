53f7a8c7cfefb64281c15b870027710fe0f2b54c
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-06 17:21:11 +0800

    spacing

--- pages/shops/ShopDetail.js
+++ pages/shops/ShopDetail.js
@@ -58,10 +58,12 @@ export default function ShopDetail({
         <p className="m-2 text-sm text-[#7b80b3]">{description}</p>
 
         {/* Buttons */}
-        <div className="flex flex-row">
-          <button className="m-auto mt-3 mb-3 flex flex-row items-center justify-center  rounded-xl bg-[#5865F2] pt-2 pb-2 pl-6 pr-10 shadow-md shadow-[#5865f28a] duration-300 ease-linear hover:bg-[#424bb6] md:m-2">
+        <div className="mt-10 flex flex-row ">
+          <button className="m-auto mt-20 mb-3 flex flex-row items-center justify-center  rounded-xl bg-[#5865F2] pt-2 pb-2 pl-6 pr-10 shadow-md shadow-[#5865f28a] duration-300 ease-linear hover:bg-[#424bb6] md:m-2">
             <FaPlay className="animate-ping" size={10} color="#fff" />
-            <h1 className="text-md pl-4 font-semibold text-white">Vote</h1>
+            <h1 className="text-md flex pl-4 font-semibold text-white">
+              Vote for more Reward Points
+            </h1>
           </button>
         </div>
       </div>
