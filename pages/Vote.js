50233f14bddd980d123b5009349b4a55866af603
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-07 16:58:32 +0800

    vote

--- pages/Vote.js
+++ pages/Vote.js
@@ -6,7 +6,9 @@ export default function Vote() {
   return (
     <div className="mt-10 flex min-h-screen flex-col items-center justify-center">
       <RangeSlider3 />
-      <h1 className="mt-3 font-bold">% of Rarbit Token Allocations</h1>
+      <h1 className="mt-3 text-2xl font-bold text-[#5865F2]">
+        % of Rarbit Token Allocations
+      </h1>
       <div className="mt-10"></div>
       <RewardTable />
       <VoteButton />
