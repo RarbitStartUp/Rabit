7a2cb2f30e0ec3bdd4d78348868b9d6b3beccb68
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-07 14:40:54 +0800

    pretty slider

--- Component/RangeSlider3.js
+++ Component/RangeSlider3.js
@@ -32,7 +32,7 @@ const marks = [
     value: 20,
   },
   {
-    value: 37,
+    value: 40,
   },
   {
     value: 100,
@@ -58,7 +58,7 @@ const IOSSlider = styled(Slider)(({ theme }) => ({
     },
   },
   '& .MuiSlider-valueLabel': {
-    fontSize: 12,
+    fontSize: 40,
     fontWeight: 'normal',
     top: -6,
     backgroundColor: 'unset',
@@ -89,91 +89,6 @@ const IOSSlider = styled(Slider)(({ theme }) => ({
   },
 }))
 
-// const PrettoSlider = styled(Slider)({
-//   color: '#52af77',
-//   height: 8,
-//   '& .MuiSlider-track': {
-//     border: 'none',
-//   },
-//   '& .MuiSlider-thumb': {
-//     height: 24,
-//     width: 24,
-//     backgroundColor: '#fff',
-//     border: '2px solid currentColor',
-//     '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-//       boxShadow: 'inherit',
-//     },
-//     '&:before': {
-//       display: 'none',
-//     },
-//   },
-//   '& .MuiSlider-valueLabel': {
-//     lineHeight: 1.2,
-//     fontSize: 12,
-//     background: 'unset',
-//     padding: 0,
-//     width: 32,
-//     height: 32,
-//     borderRadius: '50% 50% 50% 0',
-//     backgroundColor: '#52af77',
-//     transformOrigin: 'bottom left',
-//     transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-//     '&:before': { display: 'none' },
-//     '&.MuiSlider-valueLabelOpen': {
-//       transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-//     },
-//     '& > *': {
-//       transform: 'rotate(45deg)',
-//     },
-//   },
-// })
-
-// const AirbnbSlider = styled(Slider)(({ theme }) => ({
-//   color: '#3a8589',
-//   height: 3,
-//   padding: '13px 0',
-//   '& .MuiSlider-thumb': {
-//     height: 27,
-//     width: 27,
-//     backgroundColor: '#fff',
-//     border: '1px solid currentColor',
-//     '&:hover': {
-//       boxShadow: '0 0 0 8px rgba(58, 133, 137, 0.16)',
-//     },
-//     '& .airbnb-bar': {
-//       height: 9,
-//       width: 1,
-//       backgroundColor: 'currentColor',
-//       marginLeft: 1,
-//       marginRight: 1,
-//     },
-//   },
-//   '& .MuiSlider-track': {
-//     height: 3,
-//   },
-//   '& .MuiSlider-rail': {
-//     color: theme.palette.mode === 'dark' ? '#bfbfbf' : '#d8d8d8',
-//     opacity: theme.palette.mode === 'dark' ? undefined : 1,
-//     height: 3,
-//   },
-// }))
-
-// function AirbnbThumbComponent(props) {
-//   const { children, ...other } = props
-//   return (
-//     <SliderThumb {...other}>
-//       {children}
-//       <span className="airbnb-bar" />
-//       <span className="airbnb-bar" />
-//       <span className="airbnb-bar" />
-//     </SliderThumb>
-//   )
-// }
-
-// AirbnbThumbComponent.propTypes = {
-//   children: PropTypes.node,
-// }
-
 export default function CustomizedSlider() {
   return (
     <Box sx={{ width: 320 }}>
@@ -184,32 +99,6 @@ export default function CustomizedSlider() {
         marks={marks}
         valueLabelDisplay="on"
       />
-      {/* <Box sx={{ m: 3 }} />
-      <Typography gutterBottom>pretto.fr</Typography>
-      <PrettoSlider
-        valueLabelDisplay="auto"
-        aria-label="pretto slider"
-        defaultValue={20}
-      />
-      <Box sx={{ m: 3 }} /> */}
-      {/* <Typography gutterBottom>Tooltip value label</Typography>
-      <Slider
-        valueLabelDisplay="auto"
-        components={{
-          ValueLabel: ValueLabelComponent,
-        }}
-        aria-label="custom thumb label"
-        defaultValue={20}
-      /> */}
-      {/* <Box sx={{ m: 3 }} />
-      <Typography gutterBottom>Airbnb</Typography>
-      <AirbnbSlider
-        components={{ Thumb: AirbnbThumbComponent }}
-        getAriaLabel={(index) =>
-          index === 0 ? 'Minimum price' : 'Maximum price'
-        }
-        defaultValue={[20, 40]}
-      /> */}
     </Box>
   )
 }
