2350e5f8a0218f0f7944289f3bfbfa3079e93da9
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-07 16:51:54 +0800

    vote

--- pages/Vote.js
+++ pages/Vote.js
@@ -4,8 +4,9 @@ import VoteButton from '../components/VoteButton'
 
 export default function Vote() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
+    <div className="mt-10 flex min-h-screen flex-col items-center justify-center">
       <RangeSlider3 />
+      <h1 className="mt-3 font-bold">% of Rarbit Token Allocations</h1>
       <div className="mt-10"></div>
       <RewardTable />
       <VoteButton />
