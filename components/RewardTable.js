755ea3c28c2e1eadf63493abdf6ce3ff00b7af30
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-07 16:43:11 +0800

    god damn table

--- components/RewardTable.js
+++ components/RewardTable.js
@@ -12,6 +12,7 @@ const columns = [
   { field: 'shopName', headerName: 'Shop Name', width: 130 },
   { field: 'date', headerName: 'Date', width: 130 },
   { field: 'token', headerName: 'Token %', width: 130 },
+  { field: 'lockup', headerName: 'Lock Up til', width: 130 },
 
   // {
   //   field: 'fullName',
@@ -25,15 +26,69 @@ const columns = [
 ]
 
 const rows = [
-  { id: 1, shopName: 'Sora Ramen', token: '10', date: '5/1/2020' },
-  { id: 2, shopName: 'Rim Cafe', token: '20', date: '4/2/2020' },
-  { id: 3, shopName: 'Aqua Bar', token: '50', date: '8/7/2021' },
-  { id: 4, shopName: 'Steak', token: '45', date: '5/7/2021' },
-  { id: 5, shopName: 'Sushiro', token: '67', date: '6/7/2020' },
-  { id: 6, shopName: 'Pizza Express', token: '80', date: '1/8/2021' },
-  { id: 7, shopName: 'PastaHolic', token: '15', date: '15/9/2021' },
-  { id: 8, shopName: 'Spagetti', token: '5', date: '20/8/2021' },
-  { id: 9, shopName: 'MeatFever', token: '20', date: '7/8/2021' },
+  {
+    id: 1,
+    shopName: 'Sora Ramen',
+    token: '10',
+    date: '5/1/2020',
+    lockup: '5/2/2020',
+  },
+  {
+    id: 2,
+    shopName: 'Rim Cafe',
+    token: '20',
+    date: '4/2/2020',
+    lockup: '4/3/2020',
+  },
+  {
+    id: 3,
+    shopName: 'Aqua Bar',
+    token: '50',
+    date: '8/7/2021',
+    lockup: '8/8/2020',
+  },
+  {
+    id: 4,
+    shopName: 'Steak',
+    token: '45',
+    date: '5/7/2021',
+    lockup: '5/8/2020',
+  },
+  {
+    id: 5,
+    shopName: 'Sushiro',
+    token: '67',
+    date: '6/7/2020',
+    lockup: '6/8/2020',
+  },
+  {
+    id: 6,
+    shopName: 'Pizza Express',
+    token: '80',
+    date: '1/8/2021',
+    lockup: '1/9/2020',
+  },
+  {
+    id: 7,
+    shopName: 'PastaHolic',
+    token: '15',
+    date: '15/9/2021',
+    lockup: '15/10/2020',
+  },
+  {
+    id: 8,
+    shopName: 'Spagetti',
+    token: '5',
+    date: '20/8/2021',
+    lockup: '20/9/2020',
+  },
+  {
+    id: 9,
+    shopName: 'MeatFever',
+    token: '20',
+    date: '7/8/2021',
+    lockup: '7/9/2020',
+  },
 ]
 
 export default function RewardTable() {
--- pages/Vote.js
+++ pages/Vote.js
@@ -1,12 +1,12 @@
 import RangeSlider3 from '../components/RangeSlider3'
-import RewardTable2 from '../components/RewardTable2'
+import RewardTable from '../components/RewardTable'
 
 export default function Vote() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <RangeSlider3 />
       <div className="mt-10"></div>
-      <RewardTable2 />
+      <RewardTable />
     </div>
   )
 }
