eb446fdce25594a759db09c3cc1f6cf453813187
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-07 13:18:57 +0800

    slider

--- pages/Vote.js
+++ pages/Vote.js
@@ -1,16 +1,18 @@
 import RangeSlider from '../Component/RangeSlider'
+import RangeSlider2 from '../Component/RangeSlider2'
 
 export default function Vote() {
   return (
     <div>
-      <RangeSlider
+      <RangeSlider2 />
+      {/* <RangeSlider
         initialMin={10}
         // initialMax={100}
         min={0}
         max={100}
         step={0}
         // priceCap={50}
-      />
+      /> */}
     </div>
   )
 }
