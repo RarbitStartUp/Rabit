295d897f53dcb4bdbc34148a711a3a89036ef700
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-09 20:24:41 +0800

    Tabs

---
+++ pages/Records.js
@@ -0,0 +1,13 @@
+import Dropdown from '../components/DropDown'
+import RewardTable2 from '../components/RewardTable2'
+
+export default function Records() {
+  return (
+    <div className="mt-5 flex flex-col items-center justify-center">
+      <h1 className="mt-3 text-2xl font-bold text-[#5865F2]">Records</h1>
+      <div className="mt-10"></div>
+      {/* <Dropdown /> */}
+      <RewardTable2 />
+    </div>
+  )
+}
