57be60cb8a2b6d25bda4f7dce945e5ed958e0eb0
Rarbit <rarbitstartup@gmail.com>
2022-05-09 18:22:50 +0800

    Revert "Merge branch 'main' of https://github.com/RarbitStartUp/Rarbit"
    This reverts commit 859770c0779a34f438edc393bd14c823698db644, reversing
    changes made to e537842d8bb7fca029caaeab3d966414c3f6ff11.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Login from './Login'
 
 const Home: NextPage = () => {
   return (
-<<<<<<< HEAD
     <>
       <div>
         <Head>
@@ -15,18 +14,6 @@ const Home: NextPage = () => {
       </div>
     </>
 
-=======
-    <div
-    // className="flex min-h-screen flex-col items-center justify-center py-2"
-    >
-      <Head>
-        <title>Rarbit</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Login />
-      {/* <Testing /> */}
-    </div>
->>>>>>> 4e85b244f9da251778e9b2d91dfb72323fb48439
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,11 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'class',
   theme: {
     extend: {},
     fontFamily: {
       vali: ['VALORANT'],
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    // require('flowbite/plugin'),
-    // require('@tailwindcss/typography'),
-    // require('@tailwindcss/line-clamp'),
-    // require('@tailwindcss/aspect-ratio')
-  ],
+  plugins: [require('@tailwindcss/forms')],
 }
