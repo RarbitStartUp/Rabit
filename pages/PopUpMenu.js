83dc2ab119ded6f4f607c7f3ec954e5eea21e754
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-13 21:33:07 +0800

    PopUpMenu

--- pages/PopUpMenu.js
+++ pages/PopUpMenu.js
@@ -68,57 +68,12 @@ export default function PopUpMenu() {
                       aria-hidden="true"
                     />
                   )}
-                  Duplicate
+                  Language
                 </button>
               )}
             </Menu.Item>
           </div>
-          <div className="px-1 py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  {active ? (
-                    <ArchiveActiveIcon
-                      className="mr-2 h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  ) : (
-                    <ArchiveInactiveIcon
-                      className="mr-2 h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  )}
-                  Archive
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                >
-                  {active ? (
-                    <MoveActiveIcon
-                      className="mr-2 h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  ) : (
-                    <MoveInactiveIcon
-                      className="mr-2 h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  )}
-                  Move
-                </button>
-              )}
-            </Menu.Item>
-          </div>
+
           <div className="px-1 py-1">
             <Menu.Item>
               {({ active }) => (
@@ -138,7 +93,7 @@ export default function PopUpMenu() {
                       aria-hidden="true"
                     />
                   )}
-                  Delete
+                  Logout
                 </button>
               )}
             </Menu.Item>
