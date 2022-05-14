a7d9863a200566d761e1ff01bcb2a3f8cd6e99c0
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-14 23:16:50 +0800

    tell me why

--- data/dataShops.js
+++ data/dataShops.js
@@ -16,8 +16,8 @@ export const dataShops = [
     categories: 'Cafe',
     imagePath:
       'https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1937&q=80',
-    rewardPoints: 2032,
-    tokenAllocations: 20,
+    rewardPoints: '2032',
+    tokenAllocations: '20',
     location: 'Tsim Sha Shui',
     description: 'Best Coffee in the City',
   },
@@ -27,7 +27,7 @@ export const dataShops = [
     categories: 'Bar',
     imagePath:
       'https://images.unsplash.com/photo-1543007630-9710e4a00a20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',
-    rewardPoints: 1298,
+    rewardPoints: '1298',
     tokenAllocations: '15',
     location: 'Central',
     description: 'Top Bar in Hong Kong',
