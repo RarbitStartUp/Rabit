59447059230986d0451f424dc49ae909ae0da47d
Exotic Coder <johnathanbeth001@gmail.com>
2022-05-29 21:49:47 +0800

    dropdown menu

--- components/DropDownMenu.js
+++ components/DropDownMenu.js
@@ -10,26 +10,25 @@ export default function PopUpMenu() {
     // <div className="w-106 top-106 fixed text-right">
     <Menu as="div" className="relative inline-block text-left">
       <div className="flex-row ">
-         <Menu.Button className="inline-flex w-[8rem] justify-center rounded-md bg-slate-100 bg-opacity-90 px-4 py-2 text-sm font-medium text-gray-600 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <Menu.Button className="inline-flex w-[8rem] justify-center rounded-md bg-slate-100 bg-opacity-90 px-4 py-2 text-sm font-medium text-gray-600 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
           <div className="my-1 flex flex-row items-center justify-center space-x-3">
-              USD
-              <div className="ml-3">
-            <Image
-            src="/img/flag/us-flag.webp"
-            alt="thumbnail"
-            width={15}
-            height={10}
-            // layout="fill"
-            objectFit="cover"
-            className="ml-5"
-          />
-
-              </div>
-            <ChevronDownIcon 
+            USD
+            <div className="ml-3">
+              <Image
+                src="/img/flag/us-flag.webp"
+                alt="thumbnail"
+                width={15}
+                height={10}
+                // layout="fill"
+                objectFit="cover"
+                className="ml-5"
+              />
+            </div>
+            <ChevronDownIcon
               className="mx-2 h-5 w-5 text-violet-200 hover:text-violet-100"
               aria-hidden="true"
             />
-        </div>  
+          </div>
         </Menu.Button>
       </div>
       <Transition
@@ -41,21 +40,33 @@ export default function PopUpMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute left-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
                 <button
                   className={`${
-                    (active ? 'bg-violet-500 text-white' : 'text-violet-500', )
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    active
+                      ? 'bg-violet-500 text-white'
+                      : 'bg-slate-100 text-gray-600'
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
                 >
-                  {active ? (
+                  {/* {active ? (
                     <MdHistory className="mr-2 h-5 w-5" aria-hidden="true" />
                   ) : (
                     <MdHistory className="mr-2 h-5 w-5" aria-hidden="true" />
-                  )}
+                  )} */}
                   USD
+                  <div className="ml-3" />
+                  <Image
+                    src="/img/flag/us-flag.webp"
+                    alt="thumbnail"
+                    width={15}
+                    height={10}
+                    // layout="fill"
+                    objectFit="cover"
+                    className="ml-5"
+                  />
                 </button>
               )}
             </Menu.Item>
@@ -63,21 +74,31 @@ export default function PopUpMenu() {
               {({ active }) => (
                 <button
                   className={`${
-                    active ? 'bg-violet-500 text-white' : 'text-violet-500 '
-                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    active
+                      ? 'bg-violet-500 text-white'
+                      : 'bg-slate-100 text-gray-600'
+                  } group flex w-full items-center rounded-md px-4 py-2 text-sm`}
                 >
-                  {active ? (
+                  {/* {active ? (
                     <MdLanguage className="mr-2 h-5 w-5" aria-hidden="true" />
                   ) : (
                     <MdLanguage className="mr-2 h-5 w-5" aria-hidden="true" />
-                  )}
+                  )} */}
                   HKD
+                  <div className="ml-3" />
+                  <Image
+                    src="/img/flag/hk-flag.png"
+                    alt="thumbnail"
+                    width={15}
+                    height={10}
+                    // layout="fill"
+                    objectFit="cover"
+                    className="ml-5"
+                  />
                 </button>
               )}
             </Menu.Item>
           </div>
-
-         
         </Menu.Items>
       </Transition>
     </Menu>
