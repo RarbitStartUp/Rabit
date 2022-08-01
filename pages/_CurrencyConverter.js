6e6f3a91a2d5a227acf7dd8b6a5ec1c342ade1c1
Exotic Coder <johnathanbeth001@gmail.com>
2022-08-01 11:37:20 +0800

    converter

--- pages/_CurrencyConverter.js
+++ pages/_CurrencyConverter.js
@@ -3,10 +3,9 @@ import axios from 'axios'
 import Input from '../components/Input'
 
 export default function Home({ symbols }) {
-  // const [convertFrom, setConvertFrom] = useState('USD')
-  const amount = 100
-  const [convertTo, setConvertTo] = useState('USD')
-  // const [amount, setAmount] = useState(null)
+  const [convertFrom, setConvertFrom] = useState('ANG')
+  const [convertTo, setConvertTo] = useState('ANG')
+  const [amount, setAmount] = useState(null)
   const [convertedAmount, setConvertedAmount] = useState(null)
   /**
    *
@@ -17,7 +16,7 @@ export default function Home({ symbols }) {
     const options = {
       method: 'GET',
       url: 'http://localhost:3000/api/CurrencyConverter/convert',
-      params: { convertTo, amount },
+      params: { convertFrom, convertTo, amount },
     }
     axios
       .request(options)
@@ -31,26 +30,32 @@ export default function Home({ symbols }) {
   }
   return (
     <div className="flex flex-col items-center">
+      <h2 className=" pt-20 pb-6 text-6xl font-bold text-primary md:text-3xl">
+        Currency <span className="text-danger">Conversion</span> App
+      </h2>
+      <h3 className=" mb-14 text-2xl font-bold uppercase tracking-wide text-secondary md:px-4 md:text-center md:text-base">
+        Convert Different Concurrencies Quickly
+      </h3>
       <div className="flex w-3/6 flex-col justify-between md:w-5/6">
-        {/* <Input
+        <Input
           label="Convert from"
           dropdown={true}
           onChange={setConvertFrom}
           symbols={symbols}
-        /> */}
+        />
         <Input
           label="To Currency"
           dropdown={true}
-          onChange={setConvertTo}
+          onChange={(setConvertTo, convertCurrency)}
+          onClick={convertCurrency}
           symbols={symbols}
-          // onClick={convertCurrency}
         />
-        {/* <Input
+        <Input
           label="Your Amount"
           dropdown={false}
           onChange={setAmount}
           symbols={{}}
-        /> */}
+        />
         <button
           className=" pointer w-2/5 rounded border-none bg-danger py-2 uppercase outline-none transition duration-300 hover:shadow-custom"
           onClick={convertCurrency}
@@ -59,9 +64,9 @@ export default function Home({ symbols }) {
         </button>
         {convertedAmount && (
           <div className="my-6 flex w-3/5 rounded border-primary text-primary md:w-full">
-            <p className="text-lg font-bold uppercase tracking-wider md:text-base">
+            <p className=" text-lg font-bold uppercase tracking-wider md:text-base">
               Converted Amount:{' '}
-              <span className="text-blue">{`${convertedAmount} ${convertTo}`}</span>
+              <span className="text-danger">{`${convertedAmount} ${convertTo}`}</span>
             </p>
           </div>
         )}
