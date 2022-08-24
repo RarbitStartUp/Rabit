377d726eeca5f1a6655feb254d1c86090a44fb76
Exotic Coder <johnathanbeth001@gmail.com>
2022-08-24 14:22:40 +0800

    round to 2 decimal places

--- components/CoinItem.js
+++ components/CoinItem.js
@@ -4,6 +4,28 @@ export default function CoinItem({ coins }) {
   //   var coins = coins.filter((coin) => coin.symbol.toString() == 'sol')
   //   var coins = coins[0]
   const earning = 10
+  const round10 = (value, exp) => decimalAdjust('round', value, exp)
+
+  function decimalAdjust(type, value, exp) {
+    type = String(type)
+    if (!['round', 'floor', 'ceil'].includes(type)) {
+      throw new TypeError(
+        "The type of decimal adjustment must be one of 'round', 'floor', or 'ceil'."
+      )
+    }
+    exp = Number(exp)
+    value = Number(value)
+    if (exp % 1 !== 0 || Number.isNaN(value)) {
+      return NaN
+    } else if (exp === 0) {
+      return Math[type](value)
+    }
+    const [magnitude, exponent = 0] = value.toString().split('e')
+    const adjustedValue = Math[type](`${magnitude}e${exponent - exp}`)
+    // Shift back
+    const [newMagnitude, newExponent = 0] = adjustedValue.toString().split('e')
+    return Number(`${newMagnitude}e${+newExponent + exp}`)
+  }
 
   return (
     <div>
@@ -14,7 +36,9 @@ export default function CoinItem({ coins }) {
         </p>
         <p className="pr-3 pt-3 text-xl text-primary">=</p>
         <p className="pr-3 pt-3 text-xl text-primary">
-          {Math.floor(earning * coins.current_price)}
+          {round10(earning * coins.current_price, -2)}
+          {/* {Math.floor(earning * coins.current_price)} */}
+          {/* {earning * coins.current_price} */}
         </p>
         <p className="text-md pt-4 pr-5 font-semibold uppercase text-primary">
           USD
@@ -36,7 +60,7 @@ export default function CoinItem({ coins }) {
           *1 rarbit = 0.5 sol
         </div>
         <div class="mt-1 text-xs leading-relaxed text-slate-500 dark:text-slate-500">
-          *1 sol = {coins.current_price} usd
+          *1 sol = {round10(coins.current_price, -2)} usd
         </div>
       </div>
     </div>
