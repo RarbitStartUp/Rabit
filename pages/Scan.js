f2f2ae8916f161348a3ee8604d7237a022bb6240
Exotic Coder <johnathanbeth001@gmail.com>
2022-09-08 23:29:41 +0800

    QRCode Page

--- pages/Scan.js
+++ pages/Scan.js
@@ -8,19 +8,18 @@ export default function Scan() {
     <div>
       <QRCodeScanner />
       <div class="mx-4 mt-4 flex flex-1 flex-col justify-center rounded-2xl bg-white p-8 shadow">
-        <div class="flex items-center">
-          <h className="text-center text-primary">
+        <div class="items-center">
+          <h className="flex text-center text-primary">
             You can Scan the QR Code on the receipts provided by the Merchants
             using Rarbit POS ( Point of Sale ) system to receive Rarbit Reward
             Points
           </h>
+          <p className="py-3 text-center text-sm text-slate-400">
+            Contact us to inquire about Rarbit POS
+          </p>
         </div>
       </div>
       <div className="py-3"></div>
-      <h className="flex px-4 text-center text-sm text-slate-400">
-        Please contact rarbit_cs@gmail.com to inquire about Rarbit POS ( Point
-        of Sale ) system for your business growth
-      </h>
       <div className="absolute bottom-16 right-4">
         <Image src="/img/RarbitLogo/RarbitLogo.png" width={80} height={80} />
       </div>
