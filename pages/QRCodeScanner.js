9ae6f30fbf380db9f355bc817617f8de025ace90
Exotic Coder <johnathanbeth001@gmail.com>
2022-09-10 01:08:16 +0800

    QRCode Scanner Page

--- pages/QRCodeScanner.js
+++ pages/QRCodeScanner.js
@@ -17,6 +17,7 @@ export default function QRCodeScanner(props) {
           }
         }}
         style={{ width: '100%' }}
+        constraints={(facingMode = 'environment')}
       />
       <p>{data}</p>
     </>
