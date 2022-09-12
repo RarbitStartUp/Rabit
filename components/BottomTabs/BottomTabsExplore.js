e9c8731130e887a598c59b4e1b73f8a89637878d
Exotic Coder <johnathanbeth001@gmail.com>
2022-09-13 03:00:21 +0800

    spacing testing

--- components/BottomTabs/BottomTabsExplore.js
+++ components/BottomTabs/BottomTabsExplore.js
@@ -7,7 +7,7 @@ export default function BottomTabs() {
     <div>
       <nav class="fixed inset-x-0 bottom-0 z-10 flex justify-between bg-white/100 text-sm text-secondary shadow-inner">
         <Link href="/Reward">
-          <a class="block w-full rounded-full py-4 px-3 text-center hover:text-primary">
+          <a class="mx-3 block w-full rounded-full py-4 text-center hover:text-primary">
             <div className="flex flex-col items-center justify-center">
               <MdRedeem size={20} />
               <div className="text-xs">Reward</div>
@@ -16,7 +16,7 @@ export default function BottomTabs() {
         </Link>
 
         <Link href="/Scan">
-          <a class="block w-full py-4 px-3 text-center hover:text-primary">
+          <a class="mx-3 block w-full rounded-full py-4 text-center hover:text-primary">
             <div className="flex flex-col items-center justify-center">
               <BsFillCameraFill size={20} />
               <div className="text-xs">Scan</div>
@@ -25,7 +25,7 @@ export default function BottomTabs() {
         </Link>
 
         <Link href="/Tabs">
-          <a class="block w-full py-4 px-3 text-center text-primary hover:text-primary">
+          <a class="mx-3 block w-full rounded-full py-4 text-center text-primary hover:text-primary">
             <div className="flex flex-col items-center justify-center">
               <MdOutlineExplore size={20} />
               <div className="text-xs">Explore</div>
