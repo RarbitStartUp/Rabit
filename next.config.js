6953d2b56ce025beae80e57d8c470a2bb0d3dab7
Exotic Coder <johnathanbeth001@gmail.com>
2022-10-08 00:44:28 +0800

    config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+// })
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     domains: [
+//       'images.unsplash.com',
+//       'platform-lookaside.fbsbx.com',
+//       'firebasestorage.googleapis.com',
+//       'www.worldatlas.com',
+//     ],
+//   },
+// })
+
+const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching,
-})
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -17,5 +30,10 @@ module.exports = withPWA({
       'firebasestorage.googleapis.com',
       'www.worldatlas.com',
     ],
+  },
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
   },
 })
