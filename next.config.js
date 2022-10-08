f19a665d0a3be2c6075ac0502432a75084efb7ba
Exotic Coder <johnathanbeth001@gmail.com>
2022-10-08 18:57:29 +0800

    try

--- next.config.js
+++ next.config.js
@@ -1,50 +1,10 @@
 /** @type {import('next').NextConfig} */
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
-// })
-
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   images: {
-//     domains: [
-//       'images.unsplash.com',
-//       'platform-lookaside.fbsbx.com',
-//       'firebasestorage.googleapis.com',
-//       'www.worldatlas.com',
-//     ],
-//   },
-// })
-
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withTM = require('next-transpile-modules')([
-  '@blocto/sdk',
-  '@project-serum/sol-wallet-adapter',
-  '@solana/wallet-adapter-base',
-  '@solana/wallet-adapter-react',
-  '@solana/wallet-adapter-bitkeep',
-  '@solana/wallet-adapter-bitpie',
-  '@solana/wallet-adapter-blocto',
-  '@solana/wallet-adapter-clover',
-  '@solana/wallet-adapter-coin98',
-  '@solana/wallet-adapter-coinhub',
-  '@solana/wallet-adapter-ledger',
-  '@solana/wallet-adapter-mathwallet',
-  '@solana/wallet-adapter-phantom',
-  '@solana/wallet-adapter-safepal',
-  '@solana/wallet-adapter-slope',
-  '@solana/wallet-adapter-solflare',
-  '@solana/wallet-adapter-sollet',
-  '@solana/wallet-adapter-solong',
-  '@solana/wallet-adapter-tokenpocket',
-  '@solana/wallet-adapter-torus',
-  '@solana/wallet-adapter-wallets',
-])
-
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -56,12 +16,27 @@ module.exports = withPWA({
       'www.worldatlas.com',
     ],
   },
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    webpack5: true,
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
 })
+
+// const withPWA = require('next-pwa')
+// const runtimeCaching = require('next-pwa/cache')
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     domains: [
+//       'images.unsplash.com',
+//       'platform-lookaside.fbsbx.com',
+//       'firebasestorage.googleapis.com',
+//       'www.worldatlas.com',
+//     ],
+//   },
+//   pwa: {
+//     dest: 'public',
+//     runtimeCaching,
+//     webpack5: true,
+//     register: true,
+//     skipWaiting: true,
+//     disable: process.env.NODE_ENV === 'development',
+//   },
+// })
