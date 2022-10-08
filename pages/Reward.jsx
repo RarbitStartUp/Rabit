fd76abe74f4fa1725949f43d9ae639dfc89cfd27
Exotic Coder <johnathanbeth001@gmail.com>
2022-10-08 19:42:39 +0800

    endless

--- pages/Reward.jsx
+++ pages/Reward.jsx
@@ -22,52 +22,54 @@ const idl = require('../public/idl.json')
 const utf8 = utils.bytes.utf8
 
 // const Home: NextPage = () => {
-
-async function sendTransaction() {
-  const anchorWallet = useAnchorWallet()
-  if (!anchorWallet) {
-    return
-  }
-  const network = 'http://127.0.0.1:8899'
-  const connection = new Connection(network, 'processed')
-  const provider = new AnchorProvider(connection, anchorWallet, {
-    preflightCommitment: 'processed',
-  })
-  const program = new Program(idl, idl.metadata.address, provider)
-
-  try {
-    const toKey = new PublicKey('7k4uCFsyTDpSmBpBfminAMFzrfsrap7TX2QGBwwbi1Qm')
-    const [escrowPDA] = await web3.PublicKey.findProgramAddress(
-      [
-        utf8.encode('escrow'),
-        anchorWallet.publicKey.toBuffer(),
-        toKey.toBuffer(),
-      ],
-      program.programId
-    )
-
-    console.log('escrowPDA', escrowPDA)
-
-    const trans = await program.methods
-      .createEscrow(new BN(30))
-      .accounts({
-        escrow: escrowPDA,
-        from: anchorWallet.publicKey,
-        to: toKey,
-        systemProgram: web3.SystemProgram.programId,
-      })
-      .rpc()
-
-    console.log('trans', trans)
-
-    const escrowAccount = await program.account.escrowAccount.fetch(escrowPDA)
-    console.log('escrowAccount', escrowAccount)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
 export default function Reward() {
+  async function sendTransaction() {
+    const anchorWallet = useAnchorWallet()
+    if (!anchorWallet) {
+      return
+    }
+    const network = 'http://127.0.0.1:8899'
+    const connection = new Connection(network, 'processed')
+    const provider = new AnchorProvider(connection, anchorWallet, {
+      preflightCommitment: 'processed',
+    })
+    const program = new Program(idl, idl.metadata.address, provider)
+
+    try {
+      const toKey = new PublicKey(
+        '7k4uCFsyTDpSmBpBfminAMFzrfsrap7TX2QGBwwbi1Qm'
+      )
+      const [escrowPDA] = await web3.PublicKey.findProgramAddress(
+        [
+          utf8.encode('escrow'),
+          anchorWallet.publicKey.toBuffer(),
+          toKey.toBuffer(),
+        ],
+        program.programId
+      )
+
+      console.log('escrowPDA', escrowPDA)
+
+      const trans = await program.methods
+        .createEscrow(new BN(30))
+        .accounts({
+          escrow: escrowPDA,
+          from: anchorWallet.publicKey,
+          to: toKey,
+          systemProgram: web3.SystemProgram.programId,
+        })
+        .rpc()
+
+      console.log('trans', trans)
+
+      const escrowAccount = await program.account.escrowAccount.fetch(escrowPDA)
+      console.log('escrowAccount', escrowAccount)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  // export default function Reward() {
   const router = useRouter()
   return (
     <div>
