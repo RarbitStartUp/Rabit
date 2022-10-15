b9fc811adb7f8bd9f62260701ef4dd62dd40a118
Exotic Coder <johnathanbeth001@gmail.com>
2022-10-15 11:17:49 +0800

    try

--- pages/Reward.tsx
+++ pages/Reward.tsx
@@ -171,7 +171,7 @@ export default function Reward() {
     const params = new URLSearchParams({
       dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
       cluster: 'devnet',
-      app_url: 'https://rarbit.com',
+      app_url: 'https://rarbit.vercel.app/',
       // app_url: 'myapp://',
       redirect_link: onConnectRedirectLink,
     })
